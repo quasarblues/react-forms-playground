875caffaccb01af0a195bb931a20baf5ebd28513
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-09-20 17:01:12 +0900

    Added SignUpForm.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import UsernameForm from './UsernameForm'
+import SignUpForm from './SignUpForm'
 
 function App() {
 
   return (
     <>
-      <UsernameForm />
+      <SignUpForm />
     </>
   )
 }
---
+++ src/SignUpForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./SignUpForm.css";
+
+export default function SignUpForm() {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+
+    });
+
+    const handleChange = (e) => {
+        const changedData = e.target.name;
+        const newData = e.target.value;
+
+        setFormData(currData => {
+            currData[changedData] = e.target.value;
+            return { ...currData };
+        })
+    }
+    return (
+        <form className="SignUpForm">
+            <h2>Sign Up With Us!</h2>
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" name="firstName" id="firstName" onChange={handleChange} />
+
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" name="lastName" id="lastName" onChange={handleChange} />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" onChange={handleChange} />
+
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" onChange={handleChange} />
+        </form>
+    )
+}
