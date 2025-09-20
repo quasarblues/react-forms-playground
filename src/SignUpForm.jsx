bf9d2992432af73c18f2bf02083b431454950b35
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-09-20 18:17:30 +0900

    No logic change

--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -18,6 +18,7 @@ export default function SignUpForm() {
             return {
                 ...currData,
                 [changedData]: newData
+                // [e.target.name] = e.target.value
             };
         });
     }
